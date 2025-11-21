9c996b431bc0c1ba6d487c8bd4e86fdd87f0e912
arnii2511 <150372059+arnii2511@users.noreply.github.com>
2025-11-21 09:07:57 +0530

    Delete backend directory

--- backend/models/Registration.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const RegistrationSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  eventId: { type: mongoose.Schema.Types.ObjectId, ref: 'Event', required: true },
-  createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('Registration', RegistrationSchema);
